c066385c870568aeb3be5ad3fcb9468b551b0201
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-11-14 19:57:26 +0100

    Added new checkinboxes and new calculations

--- weightConverter.js
+++ weightConverter.js
@@ -1,16 +1,57 @@
 document.getElementById("output").style.visibility = "hidden";
+window.onload = grams();
 
-document.getElementById("lbsInput").addEventListener("input", function(e) {
+function grams() {
+      
+    document.getElementById("lbsInput").addEventListener("input", function(e) {
     
     document.getElementById("output").style.visibility = "visible";
    
     var result = e.target.value;
     console.log(result);
 
-    document.getElementById("gramsOutput").innerHTML = (result / 0.0022046).toFixed(2);
+    document.getElementById("grams").innerHTML = '<h4 class="font-weight-light">Pounds:</h4><div id="gramsOutput">'+(result / 453.592).toFixed(3)+'</div></div></div>';
     
-    document.getElementById("kgOutput").innerHTML = (result / 2.2046).toFixed(2);
+    document.getElementById("kilograms").innerHTML = '<h4 class="font-weight-light">Kilograms:</h4><div id="gramsOutput">'+(result / 1000).toFixed(3)+'</div></div></div>';
     
-    document.getElementById("ozOutput").innerHTML = (result / 16).toFixed(2);
+    document.getElementById("ounces").innerHTML = '<h4 class="font-weight-light">Ounces:</h4><div id="ounces">'+(result / 28.3495).toFixed(3)+'</div></div></div>';
+    
+}); 
+}
+
+function kilograms() {
+    
+    document.getElementsById("lbsInput").addEventListener("input", function(e) {
+    
+    document.getElementsByName('inputNumber')[0].value = '';
+    document.getElementById("output").style.visibility = "visible";
+   
+    var result = e.target.value;
+    console.log(result);
+
+    document.getElementById("grams").innerHTML = '<h4 class="font-weight-light">Grams:</h4><div id="gramsOutput">'+(result / 0.001).toFixed(4)+'</div></div></div>';
+    
+    document.getElementById("kilograms").innerHTML = '<h4 class="font-weight-light">Pounds:</h4><div id="pounds">'+(result / 0.453592).toFixed(2)+'</div></div></div>';
+    
+    document.getElementById("ounces").innerHTML = '<h4 class="font-weight-light">Ounces:</h4><div id="ounces">'+(result / 16).toFixed(3)+'</div></div></div>';
     
 });
+}
+
+function ounces() {
+
+    document.getElementById("lbsInput").addEventListener("input", function(e) {
+    
+    document.getElementById("output").style.visibility = "visible";
+   
+    var result = e.target.value;
+    console.log(result);
+
+    document.getElementById("grams").innerHTML = '<h4 class="font-weight-light">Grams:</h4><div id="gramsOutput">'+(result / 0.035274).toFixed(3)+'</div></div></div>';
+    
+    document.getElementById("kilograms").innerHTML = '<h4 class="font-weight-light">Kilograms:</h4><div id="gramsOutput">'+(result / 35.274).toFixed(3)+'</div></div></div>';
+    
+    document.getElementById("ounces").innerHTML = '<h4 class="font-weight-light">Pounds:</h4><div id="gramsOutput">'+(result / 16).toFixed(3)+'</div></div></div>';
+    
+});
+}
