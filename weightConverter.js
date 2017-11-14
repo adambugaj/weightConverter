236bd6f158ace90154ed1e179dc89d6187b7c0e7
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-11-14 17:26:25 +0100

    App is done

--- weightConverter.js
+++ weightConverter.js
@@ -1,10 +1,16 @@
-document.getElementById("output").addEventListener("input", function(e) {
-    var result = e.target.value;
+document.getElementById("output").style.visibility = "hidden";
+
+document.getElementById("lbsInput").addEventListener("input", function(e) {
     
+    document.getElementById("output").style.visibility = "visible";
+   
+    var result = e.target.value;
     console.log(result);
 
     document.getElementById("gramsOutput").innerHTML = (result / 0.0022046).toFixed(2);
     
-    document.getElementById("")
+    document.getElementById("kgOutput").innerHTML = (result / 2.2046).toFixed(2);
+    
+    document.getElementById("ozOutput").innerHTML = (result / 16).toFixed(2);
     
 });
