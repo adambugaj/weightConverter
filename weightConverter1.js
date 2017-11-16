f8034e835348902aa045317733cf6f3b01344528
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-11-16 17:29:04 +0100

    refactor to if without using functions

---
+++ weightConverter1.js
@@ -0,0 +1,35 @@
+//document.getElementById("output").style.visibility = "hidden";
+
+
+
+
+document.getElementById("inputValue").addEventListener("input", function(e){ 
+    
+    var result = e.target.value
+    console.log(document.getElementById("option1").checked === true)
+    
+    if (document.getElementById("option1").checked === true) {
+        
+        document.getElementById("grams").innerHTML = '<h4 class="font-weight-light">Pounds:</h4><div id="gramsOutput">'+(result / 453.592).toFixed(3)+'</div></div></div>';
+    
+        document.getElementById("kilograms1").innerHTML = '<h4 class="font-weight-light">Kilograms:</h4><div id="kgOutput">'+(result / 1000).toFixed(3)+'</div></div></div>';
+    
+        document.getElementById("ounces").innerHTML = '<h4 class="font-weight-light">Ounces:</h4><div id="ozOutput">'+(result / 28.3495).toFixed(3)+'</div></div></div>';
+    }
+    else if (document.getElementById("option2").checked === true) {
+        
+        document.getElementById("grams").innerHTML = '<h4 class="font-weight-light">Grams:</h4><div id="gramsOutput">'+(result / 0.001).toFixed(4)+'</div></div></div>';
+    
+        document.getElementById("kilograms1").innerHTML = '<h4 class="font-weight-light">Pounds:</h4><div id="pounds">'+(result / 0.453592).toFixed(2)+'</div></div></div>';
+    
+        document.getElementById("ounces").innerHTML = '<h4 class="font-weight-light">Ounces:</h4><div id="ozOutput">'+(result / 16).toFixed(3)+'</div></div></div>';
+    } 
+    else {
+         document.getElementById("grams").innerHTML = '<h4 class="font-weight-light">Grams:</h4><div id="gramsOutput">'+(result / 0.035274).toFixed(3)+'</div></div></div>';
+    
+        document.getElementById("kilograms1").innerHTML = '<h4 class="font-weight-light">Kilograms:</h4><div id="kgOutput">'+(result / 35.274).toFixed(3)+'</div></div></div>';
+    
+        document.getElementById("ounces").innerHTML = '<h4 class="font-weight-light">Pounds:</h4><div id="pounds">'+(result / 16).toFixed(3)+'</div></div></div>';
+    }
+    
+});
